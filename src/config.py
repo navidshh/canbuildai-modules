e96fdeaab0c9793f913ca65ffab53a6c70035370
navidshh <navid.shirzadi@nrcan-rncan.gc.ca>
2026-02-04 16:31:32 -0500

    Add weather files to repository and update config to use own repo
    - Add 8 Canadian city weather files (Calgary, Halifax, Montreal, Iqaluit, Toronto, Vancouver, Whitehorse, Winnipeg)
    - Update WEATHER_DATA_STORE to point to own repository
    - Weather files now hosted at navidshh/surrogate-api-test/src

--- src/config.py
+++ src/config.py
@@ -43,7 +43,8 @@ class AppConfig(BaseModel):
     # # URL where weather files are stored
     # WEATHER_DATA_STORE: AnyHttpUrl = 'https://raw.githubusercontent.com/NREL/openstudio-standards/nrcan/data/weather/' # NRCan's weather file but may not working.
     # WEATHER_DATA_STORE: AnyHttpUrl = 'https://raw.githubusercontent.com/bsuite365/surrogate_model_weather/main' # BSUP's weather file (old).
-    WEATHER_DATA_STORE: AnyHttpUrl = 'https://raw.githubusercontent.com/canmet-energy/btap_weather/main/historic' # CanmetENERGY BTAP weather files
+    # WEATHER_DATA_STORE: AnyHttpUrl = 'https://raw.githubusercontent.com/canmet-energy/btap_weather/main/historic' # CanmetENERGY BTAP weather files
+    WEATHER_DATA_STORE: AnyHttpUrl = 'https://raw.githubusercontent.com/navidshh/surrogate-api-test/main/src' # Own repository weather files
 
     # WEATHER_DATA_STORE: dict = {
     #     "historic": "https://github.com/canmet-energy/btap_weather/tree/main/historic",
